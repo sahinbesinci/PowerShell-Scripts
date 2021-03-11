e690467cf47ae0b51d6053136e5b6bdb448b9cfc
Şahin <sahinbesinci@gmail.com>
2021-03-11 15:09:25 +0300

    Update IIS-Install.ps1

--- IIS-Install.ps1
+++ IIS-Install.ps1
@@ -1,34 +1,5 @@
 Install-WindowsFeature -name Web-Server -IncludeManagementTools
-Add-WindowsFeature 
-Web-Http-Redirect, 
-Web-DAV-Publishing, 
-Web-Custom-Logging, 
-Web-Log-Libraries, 
-Web-ODBC-Logging, 
-Web-Request-Monitor, 
-Web-Http-Tracing, 
-Web-Dyn-Compression, 
-Web-Basic-Auth, 
-Web-CertProvider, 
-Web-Client-Auth, 
-Web-Digest-Auth, 
-Web-Cert-Auth, 
-Web-Url-Auth, 
-Web-Windows-Auth, 
-Web-App-Dev, 
-Web-Net-Ext45, 
-Web-Asp-Net45, 
-NET-WCF-HTTP-Activation45, 
-NET-WCF-Pipe-Activation45, 
-NET-WCF-TCP-Activation45, 
-RSAT, 
-RSAT-Feature-Tools, 
-RSAT-SNMP, 
-SNMP-Service, 
-WAS, 
-WAS-Process-Model, 
-WAS-Config-APIs,
-Telnet-Client 
--IncludeManagementTools
+
+Add-WindowsFeature Web-Http-Redirect, Web-DAV-Publishing, Web-Custom-Logging, Web-Log-Libraries, Web-ODBC-Logging, Web-Request-Monitor, Web-Http-Tracing, Web-Dyn-Compression, Web-Basic-Auth, Web-CertProvider, Web-Client-Auth, Web-Digest-Auth, Web-Cert-Auth, Web-Url-Auth, Web-Windows-Auth, Web-App-Dev, Web-Net-Ext45, Web-Asp-Net45, NET-WCF-HTTP-Activation45, NET-WCF-Pipe-Activation45, NET-WCF-TCP-Activation45, RSAT, RSAT-Feature-Tools, RSAT-SNMP, SNMP-Service, WAS, WAS-Process-Model, WAS-Config-APIs,Telnet-Client -IncludeManagementTools
 
 Get-windowsFeature
